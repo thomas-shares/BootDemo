051a045d0e0ca260b5d27088fe774814c5bfb3e1
Thomas van der Veen <thomas@bluefeniks.com>
2022-07-05 12:12:54 +0200

    First Entity

--- src/main/java/com/mycompany/demo/entities/Customer.java
+++ src/main/java/com/mycompany/demo/entities/Customer.java
@@ -1,10 +1,42 @@
 package com.mycompany.demo.entities;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 
+@Entity
 public class Customer {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	private Long id;
+	private String name;
+	private Date created;
+	
+	public Customer( String name) {
+		this.name = name;
+		this.created = new Date();
+	}
+	
+	public Long getId() {
+		return this.id;
+	}
 
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	public Date getCreated() {
+		return created;
+	}
+	
+	public void setCreated(Date created) {
+		this.created = created;
+	}
 }
